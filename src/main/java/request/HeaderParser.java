5bdfa45a0f31842ed00d97b3b2df6bec6bff95c0
Tania Topciov <taniatopciov@yahoo.com>
2021-10-30 17:06:01 +0300

    Added Interfaces for HeaderBuilderImpl and HeaderParserImpl

--- src/main/java/request/HeaderParser.java
+++ src/main/java/request/HeaderParser.java
@@ -3,31 +3,7 @@ package request;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class HeaderParser {
+public interface HeaderParser {
 
-    public HashMap<String, String> parseHeaders(ArrayList<String> headersList) {
-        if(headersList == null || headersList.isEmpty()) {
-            return new HashMap<>();
-        }
-
-        int index = -1;
-        HashMap<String, String> headersMap = new HashMap<>();
-
-        for (String header : headersList) {
-            if(header == null || header.isEmpty())
-            {
-                continue;
-            }
-
-            index = header.indexOf(':');
-            if(index == -1)
-            {
-                continue;
-            }
-
-            headersMap.put(header.substring(0, index).trim(), header.substring(index + 1).trim());
-        }
-
-        return headersMap;
-    }
+    public HashMap<String, String> parseHeaders(ArrayList<String> headersList);
 }
--- src/main/java/response/HeaderBuilder.java
+++ src/main/java/response/HeaderBuilder.java
@@ -1,28 +1,7 @@
 package response;
 
 import java.util.HashMap;
-import java.util.Map;
 
-public class HeaderBuilder {
-
-    public String buildHeaders(HashMap<String, String> headers) {
-        if(headers == null || headers.isEmpty())
-        {
-            return "\r\n";
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for(var entry : headers.entrySet())
-        {
-            stringBuilder.append(entry.getKey());
-            stringBuilder.append(": ");
-            stringBuilder.append(entry.getValue());
-            stringBuilder.append("\r\n");
-        }
-
-        stringBuilder.append("\r\n");
-
-        return stringBuilder.toString();
-    }
+public interface HeaderBuilder {
+    public String buildHeaders(HashMap<String, String> headers);
 }
