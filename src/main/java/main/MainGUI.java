435d0e01634eab126be64698e3048570b7b09f41
Tania Topciov <taniatopciov@yahoo.com>
2021-12-17 20:27:29 +0200

    Added JavaFX Dependencies

---
+++ src/main/java/main/MainGUI.java
@@ -0,0 +1,24 @@
+package main;
+
+import gui.ServerSettingsController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainGUI extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ServerSettingsController.class.getResource("server-settings.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
